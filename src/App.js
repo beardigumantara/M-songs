ae36e36e23a0744a3c64b33c95c133c9fdbee79b
diraa173 <gumantarab@gmail.com>
2022-03-23 12:37:52 +0700

    homework

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import data from './data';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <header>
+          <h1>Create Playlist</h1>
+        </header>
+      </div>
+      <div className="music-desc">
+        <div className="container">
+          <div className="music">
+            <img
+              className="img-icon"
+              src={data.album.images[1].url}
+              alt=""
+            />
+            <h3>Title : {data.name}</h3>
+            <p>Artists : {data.artists[0].name}</p>
+            <p>Albums : {data.album.name}</p>
+            <button class="button2" type="submit">Nothing</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
