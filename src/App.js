4699ffc105cbdc3888ac88a6bfe832eb058e1334
diraa173 <gumantarab@gmail.com>
2022-03-30 16:12:15 +0700

    Homework Modul 3 Session 1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import ButtonTrack from './components/button-props/index';
 import './App.css';
 import data from './data';
 import TrackMusic from './components/track/index';
+import axios from "axios";
+import url from './helper/auth';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [token, setToken] = useState("");
+  const [searchSong, setSearchSong] = useState("");
+  const [songData, setSongData] = useState([]);
+  
+  useEffect(() => {
+    const queryString = new URL(window.location.href.replace('#', '?')).searchParams;
+    const accessToken = queryString.get('access_token');
+    setToken(accessToken);
+  }, []);
 
-  const renderSong = data.map((music) => 
+  const getSong = async () => {
+    await axios
+      .get(`https://api.spotify.com/v1/search?q=${searchSong}&type=track&access_token=${token}`)
+      .then((response) =>{
+        setSongData(response.data.tracks.items);
+      })
+      .catch((err) =>{
+        console.log(err)
+      });
+  };
+
+  const renderSong = songData.map((music) => 
     <TrackMusic 
+      key={music.id}
       images={music.album.images[1].url}
       title={music.name}
       artist={music.artists[0].name}
@@ -18,19 +42,27 @@ function App() {
     <div className="App">
       <div className="main">
         <header>
+          <div className="navbar">
           <h1>Create Playlist</h1>
+          <a href={url}>login</a>
+          </div>
         </header>
       </div>
-      <div className="music-desc">
-        <div className="container">
-          <div className="music">
-            {renderSong}
+      <main>
+        <div className="search-bar">
+          <input type="search" onChange={(e) =>setSearchSong(e.target.value)}/>
+          <button type="button" onClick={getSong}>search</button>
+        </div>
+        <div className="music-desc">
+          <div className="container">
+            <div className="music">
+              {renderSong}
+            </div>
           </div>
         </div>
-      </div>
+      </main>
     </div>
   );
-}
+};
 
-const spotify_secret_key = process.env.REACT_APP_SPOTIFY_KEY;
 export default App;
---
+++ src/helper/auth/index.js
@@ -0,0 +1,16 @@
+const client_id = process.env.REACT_APP_CLIENT_ID;
+var redirect_uri = 'http://localhost:3000/';
+
+var state = "m-songs_Berdi";
+
+localStorage.setItem("TOKEN", state);
+var scope = 'user-read-private user-read-email';
+
+var url = 'https://accounts.spotify.com/authorize';
+url += '?response_type=token';
+url += '&client_id=' + encodeURIComponent(client_id);
+url += '&scope=' + encodeURIComponent(scope);
+url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+url += '&state=' + encodeURIComponent(state);
+
+export default url;
